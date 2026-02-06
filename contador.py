a3c421641d23b3d5f912b6b8e44dd9c68a413607
Diogo Souza <diogosouza.santus@gmail.com>
2026-02-05 21:31:47 -0300

    teste

---
+++ contador.py
@@ -0,0 +1,26 @@
+'''
+
+O que é uma estrutura de repetição?
+
+Serve para executar um bloco de código várias vezes 
+Enquanto uma condição é verdadeira
+
+
+'''
+
+'''     
+
+Lógica tabuada
+
+5*1=5
+5*2=10
+5*3=15
+
+'''
+
+tabuada = 5
+contador = 0 
+
+while contador <= 10:
+    print(tabuada, "x", contador, "=", contador * tabuada)
+    contador += 1
