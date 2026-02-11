22dc0be3cd4e9e908b8eec5c4dae95690f9c98f1
Diogo Souza <diogosouza.santus@gmail.com>
2026-02-10 22:03:45 -0300

    save.3

--- for/desafio.py
+++ for/desafio.py
@@ -1,19 +1,24 @@
-notas = [5, 6, 7, 8, 9]
+notas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 aprovados = 0
 acompanhamento = 0
-
+reprovados = 0
 
 for nota in notas:
     if nota >= 7:
      print(f"Nota {nota} - Funcionário aprovado!")
      aprovados += 1
+    elif nota >= 4:
+      print(f"Nota {nota} - Funcionário em acompanhamento")
+      acompanhamento += 1
     else:
-     print(f"Nota {nota} - Funcionário em acompanhamento")
-     acompanhamento += 1
+      print(f"Nota {nota} - Funcionário reprovado")
+      reprovados += 1
+     
+     
 
 print("\nResumo final:")
 print(f"Aprovados: {aprovados}")
 print(f"Em acompanhamento: {acompanhamento}")
-
+print(f"Reprovados: {reprovados}")
 
